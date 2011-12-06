60242e4b390afd9f0eccf36d0b534afdeee78862
Michael Wheeler <mt.maui@gmail.com>
2011-12-05 23:25:56 -0800

    Built out filter, map, and range, etc. Generator object to create asynchronous lists

--- br_stream.js
+++ br_stream.js
@@ -1,13 +1,118 @@
 // Basic streams
 // Inspired by http://blog.vjeux.com/2011/javascript/stream-lazy-iteration-with-continuation.html
 (function(context) {
-  var br_stream = {
+  var clone, br_stream, generator;
+  clone = function(obj) {
+    var F = function() {};
+    F.prototype = obj;
+    return new F;
+  };
+  generator = {
+    next: null,
+    make: function() {
+      return clone(this);
+    },
+    stop: function() {
+      throw new generator.Stopper;
+    },
+    delay: 0,
+    async: true,
+    to_stream: function(gen) {
+      gen = gen || this;
+      return br_stream.make(function stream(next) {
+        try {
+          next(gen.next());
+          
+          if(this.async) {
+            window.setTimeout(function() {
+              stream(next);
+            }, this.delay);
+          } else {                 
+            stream(next);
+          }
+        } catch(e) {
+          if(!(e instanceof generator.Stopper)) {
+            throw e;
+          }
+        }
+      });
+    },
+    Stopper: function() {}
+  };
+  br_stream = {
     empty: function empty() { },
+    cons: function(head, tail) {
+      return this.make(function(next) {
+        next(head);
+        tail.stream(next);
+      });
+    },
     stream: null,
     make: function(stream) {
+      var f = clone(this);
       
+      // override a property
+      f.stream = stream || this.empty;
+      return f;
+    }, 
+    range: function(min, max) {
+      var me = generator.make(), 
+        i = min || 0;
+      
+      me.next = function() {
+        if(typeof max === "number" && i > max) {
+          me.stop();
+        }
+        return i++;
+      };
+      
+      return me.to_stream();
+    },
+    filter: function(f, stream) {
+      stream = stream || this.stream;
+      return this.make(function(next) {
+        stream(function(val) {
+          if(f(val)) {
+            next(val);
+          }
+        })
+      });
+    },
+    map: function(f, stream) {
+      stream = stream || this.stream;
+      return this.make(function(next) {
+        stream(function(val) {
+          next(f(val));
+        });
+      });
+    },
+    reduce: function(f, initial, stream) {
+      var ret = initial;
+      stream = stream || this.stream;
+      stream(function(val){
+        ret = f(val, ret);
+      });
+      return ret;
+    },
+    toArray: function(stream) {
+      var arr = [];
+      stream = stream || this.stream;
+      
+      stream(function(val){ 
+        arr.push(val);
+      });
+      
+      return arr;
+    },
+    print: function(stream) {
+      stream = stream || this.stream;
+      
+      stream(function(val){ 
+        console.log(val)
+      });
     }
   };
   
   context.br_stream = br_stream;
+  context.generator = generator;
 }(this));
--- streams.qunit.js
+++ streams.qunit.js
@@ -4,51 +4,99 @@ $(document).ready(function() {
     teardown: function(){}
   });
   test("should be able to clone the stream object with a new stream", function() {
-    var s = br_stream.make(function() {
-      return function(next) {
-        for(var i=0, ii=3; i<ii; i++) {
-          next(i);
-        }
-      }
-    }),
-    result = s.join(",");
+    var f = function() {
+      return function() {};
+    }, 
+    s = br_stream.make(f);
     
-    equals(result, "1,2,3");
+    equals(f, s.stream, "Instance has the stream");
+    notEqual(f, br_stream.stream, "Superclass has it's own stream");
+    notEqual(s, br_stream, "Instance is not it's parent");
+    equal(s.toArray, br_stream.toArray, "parent functions untouched");
   });
-  test("can join elements of a stream into a string", function() {
-    var s = br_stream.make(function() {
-      return function(next) {
-        for(var i=0, ii=3; i<ii; i++) {
+  test("can call toArray on a stream", function() {
+    var s = br_stream.make(function(next) {
+        for(var i=1, ii=3; i<=ii; i++) {
           next(i);
         }
-      }
-    }),
-    result = s.join(",");
+      }),
+    result = s.toArray();
     
-    equals(result, "1,2,3");
+    same(result, [1,2,3], "returns an array");
+  });
+  test("can create an infinite range", function() {
+    stop();
+    var s = br_stream.range(1,3),
+      res = [],
+      exp = [1,2,3];
+      
+    s.stream(function(val) {
+      res.push(val);
+      if(res.length === 3) {
+        start();
+        same(res, [1,2,3]);
+      }
+    }); 
   });
   test("should be able to filter a stream", function() {
-    var s = br_stream.make(function() {
-      return function(next) {
-        for(var i=0, ii=3; i<ii; i++) {
+    var s = br_stream.make(function(next) {
+        for(var i=1, ii=3; i<=ii; i++) {
           next(i);
         }
-      }
+      }),
+    result = s.filter(function(val) {
+      return val % 3 === 0;
     }),
-    result = s.join(",");
-    
-    equals(result, "1,2,3");
+    arr = result.toArray();
+
+    equals(result.toArray, br_stream.toArray, "returns a new stream object");
+    same(arr, [3]);
   });
   test("should be able to call map to modify a stream", function() {
-    var s = br_stream.make(function() {
-      return function(next) {
-        for(var i=0, ii=3; i<ii; i++) {
-          next(i);
-        }
+    var s = br_stream.make(function(next) {
+      for(var i=1, ii=3; i<=ii; i++) {
+        next(i);
       }
     }),
-    result = s.join(",");
+    mapped = s.map(function(i) {
+      return i*3;
+    }),
+    result = mapped.toArray().join(",");
     
-    equals(result, "1,2,3");
+    equals(result, "3,6,9");
+  });
+  test("can reduce the stream", function() {
+    var s = br_stream.make(function(next) {
+      [1,2,3].forEach(function(v) {
+        next(v);
+      });
+    }),
+    result = s.reduce(function(a, b) {
+      return a+b;
+    }, 0);
+
+    equals(result, 6, "added up number");
+  });
+  test("can create a stream from a generator", function() {
+    var ticks = function(time) {
+      var me = generator.make(),
+        starting = new Date();
+      
+      me.next = function() {
+        var now = new Date();
+        if(now-starting >= time) {
+          me.stop();
+        }
+        return now;
+      };
+      
+      return me.to_stream();
+    }, t = ticks(2000);
+    equals(t.toArray, br_stream.toArray, "returns a stream object");
+  });
+  test("can create a functional list", function() {
+    var list = s.range(1,3).to_list();
+    
+    
   });
 });
